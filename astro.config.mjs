b2036fcfc6a5edb89228a064e102583741a25e7d
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2023-12-27 13:00:06 -0500

    Upgrade to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ryanmichaelhirst.us',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://ryanmichaelhirst.us",
+  integrations: [mdx(), sitemap(), tailwind()],
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
